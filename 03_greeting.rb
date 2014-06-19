445420c66dbf7001df3236161f76c9564b0bff3c
Rob Hill <robert.hill0122@gmail.com>
2014-06-19 13:30:12 -0600

    Changed the first few files.

--- 03_greeting.rb
+++ 03_greeting.rb
@@ -1,3 +1,5 @@
 require_relative "person"
 
 # Print the string "Hello, nice to meet you Bjorn Borg."
+p BJORN_BORG.keys
+puts "Hello, nice to meets you #{BJORN_BORG["first_name"]} #{BJORN_BORG["last_name"]}."
